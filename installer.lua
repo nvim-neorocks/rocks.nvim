2088623efab2d21390abab305aa72556697ff031
Vhyrro <vhyrro@gmail.com>
2023-10-28 11:04:30 +0200

    fix: try to make installer work

--- installer.lua
+++ installer.lua
@@ -1,9 +1,11 @@
 local buffer = vim.api.nvim_create_buf(true, true)
-vim.api.nvim_win_set_buf(0, buffer)
+vim.api.nvim_buf_set_name(buffer, "rocks.nvim installer")
 
 vim.api.nvim_create_autocmd("BufEnter", {
     buffer = buffer,
     callback = function()
-        vim.notify("Installation time")
+        vim.notify("It's installation time")
     end,
 })
+
+vim.api.nvim_win_set_buf(0, buffer)
