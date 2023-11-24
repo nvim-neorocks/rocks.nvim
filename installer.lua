aa8770e1a3c035e64e99cfe825e7281dbd37cff0
Vhyrro <vhyrro@gmail.com>
2023-11-24 16:48:52 +0100

    ref(installer): set up a luarocks installation for the user by default

--- installer.lua
+++ installer.lua
@@ -114,7 +114,8 @@ Once you start editing a value, you may exit it by pressing Enter or by clicking
 
 Rocks installation path: [install_path:50:{{vim.fs.joinpath(vim.fn.stdpath('data'), "rocks")}}]
 
-Should rocks.nvim set up luarocks?: [setup_luarocks:5:{{vim.fn.executable('luarocks') == 0}}]
+Should rocks.nvim set up luarocks?: [setup_luarocks:5:{{true}}]
+It is highly recommended that you allow rocks.nvim to set up luarocks for you.
 
 < OK >
     ]],
