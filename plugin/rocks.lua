9fc884b0e077774897f1bca7cc85f0975632c18d
Marc Jakobi <marc.jakobi@tiko.energy>
2023-12-17 15:10:05 +0100

    refactor(installer): only add rocks.nvim to rtp in install script
    This is in preparation for #86.
    For now, no behaviour has changed. `rocks.nvim` still adds all
    plugins to the rtp on startup.

--- plugin/rocks.lua
+++ plugin/rocks.lua
@@ -17,3 +17,6 @@ if not config.lazy then
         })
     end)
 end
+
+-- TODO: Add support for `opt`
+vim.opt.runtimepath:append(vim.fs.joinpath(config.rocks_path, "lib", "luarocks", "rocks-5.1", "*", "*"))
