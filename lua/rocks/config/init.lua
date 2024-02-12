3f50c460f5ca2a0fe17a138f4193b1a5c05c9715
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-02-12 09:01:01 -0400

    fix(config): add missing `generate_help_pages` configuration option in the user-facing configuration module

--- lua/rocks/config/init.lua
+++ lua/rocks/config/init.lua
@@ -19,6 +19,7 @@ local config = {}
 ---@field luarocks_binary? string Luarocks binary path. Defaults to `luarocks`.
 ---@field lazy? boolean Whether to query luarocks.org lazily. Defaults to `false`. Setting this to `true` may improve startup time, but features like auto-completion will lag initially.
 ---@field dynamic_rtp? boolean Whether to automatically add freshly installed plugins to the 'runtimepath'. Defaults to `true` for the best default experience.
+---@field generate_help_pages? boolean Whether to re-generate plugins help pages after installation/upgrade.
 
 ---@type RocksOpts | fun():RocksOpts
 vim.g.rocks_nvim = vim.g.rocks_nvim
