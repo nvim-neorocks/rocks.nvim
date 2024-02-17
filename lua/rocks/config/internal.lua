b3da7c6a7de07e1d3a931883e86b9cb1b03d729b
Vhyrro <vhyrro@gmail.com>
2024-02-17 14:53:02 +0100

    refactor(vim.env.path): move code to plugin/rocks.lua

--- lua/rocks/config/internal.lua
+++ lua/rocks/config/internal.lua
@@ -97,29 +97,6 @@ if #config.debug_info.unrecognized_configs > 0 then
     )
 end
 
----@type
----|"sysv"
----| "hpux"
----| "netbsd"
----| "linux"
----| "hurd"
----| "solaris"
----| "aix"
----| "irix"
----| "freebsd"
----| "openbsd"
----| "dragonfly"
----| "haiku"
----| "windows"
----| "cygwin"
----| "macosx"
-local current_os = assert((require("sysdetect").detect()), "[rocks]: unable to detect current operating system!")
-
-local env_path_seperator = current_os == "windows" and ";" or ":"
-
--- Append the binary directory to the system path.
-vim.env.PATH = vim.fs.joinpath(config.rocks_path, "bin") .. env_path_seperator .. vim.env.PATH
-
 return config
 
 --- config.lua ends here
--- plugin/rocks.lua
+++ plugin/rocks.lua
@@ -7,6 +7,29 @@ require("rocks.commands").create_commands()
 
 local config = require("rocks.config.internal")
 
+---@type
+---| "sysv"
+---| "hpux"
+---| "netbsd"
+---| "linux"
+---| "hurd"
+---| "solaris"
+---| "aix"
+---| "irix"
+---| "freebsd"
+---| "openbsd"
+---| "dragonfly"
+---| "haiku"
+---| "windows"
+---| "cygwin"
+---| "macosx"
+local current_os = assert((require("sysdetect").detect()), "[rocks]: unable to detect current operating system!")
+
+local env_path_seperator = current_os == "windows" and ";" or ":"
+
+-- Append the binary directory to the system path.
+vim.env.PATH = vim.fs.joinpath(config.rocks_path, "bin") .. env_path_seperator .. vim.env.PATH
+
 if not config.lazy then
     local nio = require("nio")
     nio.run(function()
