b711e1137d1a03fc43d31689e6e58b5c1828ab0f
Marc Jakobi <marc.jakobi@tiko.energy>
2024-02-23 21:11:07 +0100

    feat: add support for `:checkhealth`

--- plugin/rocks.lua
+++ plugin/rocks.lua
@@ -2,6 +2,8 @@ if vim.g.rocks_nvim_loaded then
     return
 end
 
+local nio = require("nio")
+
 -- Set up the Rocks user command
 require("rocks.commands").create_commands()
 
@@ -13,7 +15,6 @@ local env_path_seperator = vim.uv.os_uname().sysname:lower():find("windows") and
 vim.env.PATH = vim.fs.joinpath(config.rocks_path, "bin") .. env_path_seperator .. vim.env.PATH
 
 if not config.lazy then
-    local nio = require("nio")
     nio.run(function()
         local cache = require("rocks.cache")
         nio.gather({
@@ -25,5 +26,26 @@ end
 
 --- We don't want to run this async, to ensure plugins are sourced before `after/plugin`
 require("rocks.runtime").source_start_plugins()
+
+--- Neovim doesn't support `:checkhealth` for luarocks plugins.
+--- To work around this, we create a symlink in the `rocks_path` that
+--- we add to the runtimepath, so that Neovim can find health files.
+nio.run(function()
+    local log = require("rocks.log")
+    local health_link_dir = vim.fs.joinpath(config.rocks_path, "healthlink")
+    local lua_symlink_dir = vim.fs.joinpath(health_link_dir, "lua")
+    -- NOTE: nio.uv.fs_stat behaves differently than vim.uv.fs_stat
+    if not vim.uv.fs_stat(lua_symlink_dir) then
+        log.info("Creating health symlink directory.")
+        if vim.fn.mkdir(health_link_dir, "p") ~= 1 then
+            log.error("Failed to create health symlink directory.")
+        end
+        local rocks_lua_dir = vim.fs.joinpath(config.rocks_path, "share", "lua", "5.1")
+        nio.uv.fs_symlink(rocks_lua_dir, lua_symlink_dir)
+    end
+    vim.schedule(function()
+        vim.opt.runtimepath:append(health_link_dir)
+    end)
+end)
 
 vim.g.rocks_nvim_loaded = true
