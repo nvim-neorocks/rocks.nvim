b76cf553f0e65d3d3d2456bf2cd8db1543b58736
Marc Jakobi <marc.jakobi@tiko.energy>
2024-03-24 22:45:25 +0100

    tests(windows): correct lua cpath extensions

--- .github/resources/init-windows.lua
+++ .github/resources/init-windows.lua
@@ -10,8 +10,8 @@ local luarocks_path = {
 package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
 
 local luarocks_cpath = {
-    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib", "lua", "5.1", "?.so"),
-    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib64", "lua", "5.1", "?.so"),
+    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib", "lua", "5.1", "?.dll"),
+    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib64", "lua", "5.1", "?.dll"),
 }
 package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
 
