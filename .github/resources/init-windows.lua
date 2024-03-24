c9c0a6c368c9fd643d4e33d32700279ed4a06130
Marc Jakobi <marc.jakobi@tiko.energy>
2024-03-24 21:37:57 +0100

    tests(windows): add dispatchable integration test

---
+++ .github/resources/init-windows.lua
@@ -0,0 +1,17 @@
+vim.g.rocks_nvim = {
+    rocks_path = vim.fs.joinpath(vim.fn.getcwd(), "rocks"),
+}
+
+local luarocks_path = {
+    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "share", "lua", "5.1", "?.lua"),
+    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
+}
+package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
+
+local luarocks_cpath = {
+    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib", "lua", "5.1", "?.so"),
+    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib64", "lua", "5.1", "?.so"),
+}
+package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
+
+vim.opt.runtimepath:append(vim.fs.joinpath("${rocks}", "rocks.nvim-scm-1-rocks", "rocks.nvim", "*"))
