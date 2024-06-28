0e1cd0f00abb4c65de8cc9af11af24d3d7d747f2
Matthieu C. <886074+teto@users.noreply.github.com>
2024-06-28 16:13:45 +0200

    doc: add default for 'generate_help_pages'
    tried to harmoinze the display of defaults as well

--- lua/rocks/config/init.lua
+++ lua/rocks/config/init.lua
@@ -19,9 +19,9 @@ local config = {}
 ---@field luarocks_binary? string Luarocks binary path. Defaults to `{rocks_path}/bin/luarocks`.
 ---@field lazy? boolean Whether to query luarocks.org lazily. Defaults to `false`. Setting this to `true` may improve startup time, but features like auto-completion will lag initially.
 ---@field dynamic_rtp? boolean Whether to automatically add freshly installed plugins to the 'runtimepath'. Defaults to `true` for the best default experience.
----@field generate_help_pages? boolean Whether to re-generate plugins help pages after installation/upgrade.
+---@field generate_help_pages? boolean Whether to re-generate plugins help pages after installation/upgrade. Defaults to `true`.
 ---@field reinstall_dev_rocks_on_update? boolean Whether to reinstall 'dev' rocks on update (Default: `true`, as rocks.nvim cannot determine if 'dev' rocks are up to date).
----@field enable_luarocks_loader? boolean Whether to use the luarocks loader to support multiple dependencies (Default: `true`)
+---@field enable_luarocks_loader? boolean Whether to use the luarocks loader to support multiple dependencies (Default: `true`).
 ---@field luarocks_config? string Path to the luarocks config. If not set, rocks.nvim will create one in `rocks_path`. Warning: You should include the settings in the default luarocks-config.lua before overriding this.
 
 ---@type RocksOpts | fun():RocksOpts
