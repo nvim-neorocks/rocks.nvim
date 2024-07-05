09fa1a088c2932cd77fd2afb1b57cd0c070ef474
Marc Jakobi <marc.jakobi@tiko.energy>
2024-07-05 16:55:23 +0200

    chore: make luarocks loader/luarocks_config synchronous again

--- lua/rocks/loader.lua
+++ lua/rocks/loader.lua
@@ -2,59 +2,46 @@ local loader = {}
 
 local log = require("rocks.log")
 local config = require("rocks.config.internal")
-local nio = require("nio")
 
 ---@return string | nil
 local function get_luarocks_lua_dir_from_luarocks()
-    local future = nio.control.future()
-    local ok = pcall(
-        vim.system,
-        { config.luarocks_binary, "--lua-version=5.1", "which", "luarocks.loader" },
-        nil,
-        function(sc)
-            future.set(sc)
-        end
-    )
+    ---@type boolean, vim.SystemObj
+    local ok, so = pcall(vim.system, { config.luarocks_binary, "--lua-version=5.1", "which", "luarocks.loader" }, nil)
     if not ok then
         log.error(("Could not invoke luarocks at %s"):format(config.luarocks_binary))
         return
     end
     ---@type vim.SystemCompleted
-    local sc = future.wait()
+    local sc = so:wait()
     local result = sc.stdout and sc.stdout:match(vim.fs.joinpath("(%S+)", "5.1", "luarocks", "loader.lua"))
     return result
 end
 
----@type async fun():boolean
-loader.enable = nio.create(function()
+---@return boolean success
+function loader.enable()
     local luarocks_config_path = config.luarocks_config_path()
+    log.trace("Enabling luarocks loader")
     local luarocks_lua_dir = config.luarocks_binary == config.default_luarocks_binary
             and vim.fs.joinpath(config.rocks_path, "share", "lua")
         or get_luarocks_lua_dir_from_luarocks()
-    local future = nio.control.future()
-    vim.schedule(function()
-        log.trace("Enabling luarocks loader")
-        if luarocks_lua_dir then
-            package.path = package.path
-                .. ";"
-                .. table.concat({
-                    vim.fs.joinpath(luarocks_lua_dir, "5.1", "?.lua"),
-                    vim.fs.joinpath(luarocks_lua_dir, "5.1", "init.lua"),
-                }, ";")
-            vim.env.LUAROCKS_CONFIG = luarocks_config_path
-            local ok, err = pcall(require, "luarocks.loader")
-            if ok then
-                future.set(true)
-                return
-            end
-            log.error(err or "Unknown error initializing luarocks loader")
-            vim.notify("Failed to initialize luarocks loader: " .. err, vim.log.levels.WARN, {
-                title = "rocks.nvim",
-            })
+    if luarocks_lua_dir then
+        package.path = package.path
+            .. ";"
+            .. table.concat({
+                vim.fs.joinpath(luarocks_lua_dir, "5.1", "?.lua"),
+                vim.fs.joinpath(luarocks_lua_dir, "5.1", "init.lua"),
+            }, ";")
+        vim.env.LUAROCKS_CONFIG = luarocks_config_path
+        local ok, err = pcall(require, "luarocks.loader")
+        if ok then
+            return true
         end
-        future.set(false)
-    end)
-    return future.wait()
-end)
+        log.error(err or "Unknown error initializing luarocks loader")
+        vim.notify("Failed to initialize luarocks loader: " .. err, vim.log.levels.WARN, {
+            title = "rocks.nvim",
+        })
+    end
+    return false
+end
 
 return loader
