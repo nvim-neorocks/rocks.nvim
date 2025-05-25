042252a6bbb056bf43878f3eb7828bbb94f807d8
Marc Jakobi <marc.jakobi@tiko.energy>
2025-05-25 11:35:44 +0200

    feat: compatibility with nvim nightly 0.12.0-dev-434

--- lua/rocks/config/check.lua
+++ lua/rocks/config/check.lua
@@ -17,12 +17,21 @@
 
 local check = {}
 
----@param tbl table The table to validate
+---@param name string Argument name
+---@param value any Argument value
+---@param validator vim.validate.Validator
+---   - (`string|string[]`): Any value that can be returned from |lua-type()| in addition to
+---     `'callable'`: `'boolean'`, `'callable'`, `'function'`, `'nil'`, `'number'`, `'string'`, `'table'`,
+---     `'thread'`, `'userdata'`.
+---   - (`fun(val:any): boolean, string?`) A function that returns a boolean and an optional
+---     string message.
+---@param optional? boolean Argument is optional (may be omitted)
+---@param message? string message when validation fails
 ---@see vim.validate
 ---@return boolean is_valid
 ---@return string|nil error_message
-local function validate(tbl)
-    local ok, err = pcall(vim.validate, tbl)
+local function validate(name, value, validator, optional, message)
+    local ok, err = pcall(vim.validate, name, value, validator, optional, message)
     return ok or false, "Rocks: Invalid config" .. (err and ": " .. err or "")
 end
 
@@ -31,14 +40,35 @@ end
 ---@return boolean is_valid
 ---@return string|nil error_message
 function check.validate(cfg)
-    local ok, err = validate({
-        rocks_path = { cfg.rocks_path, "string" },
-        config_path = { cfg.config_path, "string" },
-        luarocks_binary = { cfg.luarocks_binary, "string" },
-        lazy = { cfg.lazy, "boolean" },
-        dynamic_rtp = { cfg.dynamic_rtp, "boolean" },
-        generate_help_pages = { cfg.generate_help_pages, "boolean" },
-    })
+    if vim.fn.has("nvim-0.11.0") ~= 1 then
+        -- TODO: The signature of vim.validate has changed and the old signature
+        -- produces deprecation warnings in nightly.
+        -- We don't want to bump the minimum required Neovim version
+        -- until the lux.nvim rewrite.
+        -- So for now, we're disabling config validation for Neovim 0.10
+        return true
+    end
+    local ok, err = validate("rocks_path", cfg.rocks_path, "string")
+    if not ok then
+        return false, err
+    end
+    ok, err = validate("config_path", cfg.config_path, "string")
+    if not ok then
+        return false, err
+    end
+    ok, err = validate("luarocks_binary", cfg.luarocks_binary, "string")
+    if not ok then
+        return false, err
+    end
+    ok, err = validate("lazy", cfg.lazy, "boolean")
+    if not ok then
+        return false, err
+    end
+    ok, err = validate("dynamic_rtp", cfg.dynamic_rtp, "boolean")
+    if not ok then
+        return false, err
+    end
+    ok, err = validate("generate_help_pages", cfg.generate_help_pages, "boolean")
     if not ok then
         return false, err
     end
